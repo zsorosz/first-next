672baa911cbdeba4e046a261a93990555cba5ba9
Zsofia <oroszi.zsofi@gmail.com>
2020-05-20 20:26:00 +0200

    Fetch posts with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-const Index = () => (
+import axios from 'axios';
+// import React, { Component } from 'react'
+
+// class Index extends Component {
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+             
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <h1>My Index Page</h1>
+//             </div>
+//         )
+//     }
+// }
+
+
+const Index = ({posts}) => {
+    return (
     <div>
         <h1>My Index Page</h1>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
     </div>
-)
+    );
+};
+
+Index.getInitialProps = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const { data } = res;
+    return { posts: data }; 
+};
 
 export default Index;
